4a90ecb1eb014418798d7403b54a218288c97873
Taedong Kim <dev.ted.kim@gmail.com>
2022-07-16 21:47:26 -0400

    feat: TypeDI integration with typeORM

--- src/loaders/DatabaseConnectionLoader.ts
+++ src/loaders/DatabaseConnectionLoader.ts
@@ -1,7 +1,9 @@
-import { Connection, createConnection } from "typeorm";
+import { Connection, createConnection, useContainer } from "typeorm";
+import { Container } from "typeorm-typedi-extensions";
 
 export async function DatabaseConnectionLoader (
 ): Promise<Connection> {
+  useContainer(Container);
   const connection: Connection = await createConnection();
   console.log("[database] connected", connection.name);
 
