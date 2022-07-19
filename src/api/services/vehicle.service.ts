f2742bb9eae361cf1ddaf27d7e1eb7573f6345de
Taedong Kim <dev.ted.kim@gmail.com>
2022-07-19 12:39:02 -0400

    feat: add vehicle service to validate VIN

---
+++ src/api/services/vehicle.service.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { Service } from "typedi";
+import { getError } from "../../utils/error";
+import { constants } from "../../common/constants";
+
+@Service()
+export class VehicleService {
+  async validateVIN(vin: string) {
+    try {
+      const url = `${constants.NHTSA.API}${vin}?${constants.NHTSA.DATA_FORMAT_QUERY}`;
+      const { data, status } = await axios.get(
+        url,
+        {
+          headers: {
+            Accept: "application/json",
+          },
+        },
+      );
+      if (status != 200) {
+        throw new Error("VehicleService failed to validateVIN");
+      }
+      if (data.Results[0].ErrorCode !== "0") {
+        return false;
+      }
+      return true;
+    } catch(error: unknown) {
+      throw getError(error);
+    }
+  }
+}
---
+++ src/common/constants.ts
@@ -0,0 +1,7 @@
+
+export const constants = {
+  NHTSA: {
+    API: "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/",
+    DATA_FORMAT_QUERY: "format=json",
+  }
+}
